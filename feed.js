bde649e3d9476a49c7388e9e570f25f9f45b4ffb
TetaTeuta <teuta.stazic@gmail.com>
2020-08-03 15:47:00 +0200

    Set up React enviroment
    Added React component to DOM and made succesfull request to API.

---
+++ feed.js
@@ -0,0 +1,55 @@
+(function() {
+  "use strict";
+
+  class Feed extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        feed: []
+      };
+    }
+
+    componentDidMount() {
+      const token =
+        "AgezoJcPNKeuaIu62UA2w1XhmIDl5QRIXguiyHBlyGJGms6kOow-W0SPIMTnoIoAm6KdEdFnUGYSItSbJlFN5C487oN3Rsw55CCayWcIJGOzGyjuHxsv3eiPHfdlyO6s6UPlC8XjlpTWYybyEmezvMInsCTr_sftdY6XFu-ui5pUw-A5D9pLNT9c2TE9j5UE1KRPAhMhA7BhmmZaZb3Y9BKq0h_tJO27EFhhODNDyQ-lDxrE3YjcMFLb7728bo7mAC_LPlEt_n_88LTjASAw2wjP982LIX_6JCPIzHe0XZUq-Y6GsMTEQ7br6gRKWCsa-iZN_vjSzJdQbekuV52MnydHj-P5dhp8OVxApQTobX663r-3";
+      axios
+        .get(
+          `https://api.madbarz.com/api/jobs/GetFeed?skip=0&skipFeatured=5&take=10`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }
+        )
+
+        .then((response) => {
+          this.setState({
+            feed: response.data
+          });
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+    render() {
+      const { feed } = this.state;
+
+      if (!feed) return <p>List is blank</p>;
+
+      return (
+        <div className="names__container">
+          {/* {feed.map((feed) => (
+            <div className="single_name" key={feed._id}>
+              {feed.byId.user}&nbsp;
+            </div>
+          ))} */}
+        </div>
+      );
+    }
+  }
+
+  ReactDOM.render(<Feed />, document.getElementById("reactRoot"));
+})();
